b09769bb437bdcbc7e6f1348792db2802faa767a
Yiheng (NUC) <development.henryquan@gmail.com>
2019-09-14 11:05:54 +1000

    update navigation bar colour

--- animeone/lib/main.dart
+++ animeone/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animeone/ui/page/home.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 /// Top level component
 class MyApp extends StatelessWidget {
@@ -14,13 +15,20 @@ class MyApp extends StatelessWidget {
     primarySwatch: Colors.pink
   );
 
-  final hour = DateTime.now().hour;
+  bool useDark = DateTime.now().hour < 17;
+
 
   @override
   Widget build(BuildContext context) {
+    // Setup navigation bar colour 
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: useDark ? Colors.grey[900] : Colors.grey[50],
+      systemNavigationBarIconBrightness: useDark ? Brightness.light : Brightness.dark
+    ));
+
     return MaterialApp(
       title: 'AnimeOne',
-      theme: hour > 17 ? darkTheme : lightTheme,
+      theme: useDark ? darkTheme : lightTheme,
       home: HomePage(),
     );
   }

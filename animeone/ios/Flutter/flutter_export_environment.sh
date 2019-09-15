89940e04f3130941c434c5b1d57f3bbe8381b5d3
Yiheng (Mac) <development.henryquan@gmail.com>
2019-09-15 11:48:05 +1000

    rebuild ios

--- animeone/ios/Flutter/flutter_export_environment.sh
+++ animeone/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Programming\Flutter\AnimeOne\animeone"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/henry/Programming/Flutter/sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/henry/Programming/Flutter/AnimeOne/animeone"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/henry/Programming/Flutter/sdk/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"

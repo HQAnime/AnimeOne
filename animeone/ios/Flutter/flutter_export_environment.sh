b99aa2ab00d3c2d071f5dc984cc8acd055510a3a
Yiheng (Mac) <development.henryquan@gmail.com>
2019-09-15 11:59:26 +1000

    running on iPadOs

--- animeone/ios/Flutter/flutter_export_environment.sh
+++ animeone/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/henry/Programming/Flutter/sdk"
 export "FLUTTER_APPLICATION_PATH=/Users/henry/Programming/Flutter/AnimeOne/animeone"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/henry/Programming/Flutter/AnimeOne/animeone/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/henry/Programming/Flutter/sdk/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/henry/Programming/Flutter/sdk/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"

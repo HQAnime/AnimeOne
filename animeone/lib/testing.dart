f2b59dd5c179825e5c0b92172aecf2826c50bd24
Yiheng (NUC) <development.henryquan@gmail.com>
2019-09-30 20:52:18 +1000

    update test

---
+++ animeone/lib/testing.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// .\lib\testing class
+class testing extends StatelessWidget {
+
+   testing({Key key}) : super(key: key);
+
+   @override
+   Widget build(BuildContext context) {
+       return Container();
+   }
+
+}

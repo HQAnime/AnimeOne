39a84078379a5eaf4c7d14e69e3d1f20a23b7852
Yiheng (NUC) <development.henryquan@gmail.com>
2019-10-05 13:48:37 +1000

    add tooltip for better accessibility

--- animeone/lib/ui/component/AnimeCoverImage.dart
+++ animeone/lib/ui/component/AnimeCoverImage.dart
@@ -13,27 +13,30 @@ class AnimeCoverImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraint) {
-        return Stack(
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 16 / 9,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset('lib/assets/cover/${video.image}.jpg'),
-              )
-            ),
-            Positioned.fill(
-              child: IconButton(
-                onPressed: () {
-                  // video.launchURL();
-                  Navigator.push(context, new MaterialPageRoute(builder: (context) => Video(video: this.video)));
-                },
-                iconSize: constraint.maxWidth / 6,
-                icon: Icon(Icons.play_circle_outline),
-                color: Colors.white,
+        return Tooltip(
+          message: '點擊進入内置視頻播放器',
+          child: Stack(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset('lib/assets/cover/${video.image}.jpg'),
+                )
               ),
-            )
-          ]
+              Positioned.fill(
+                child: IconButton(
+                  onPressed: () {
+                    // video.launchURL();
+                    Navigator.push(context, new MaterialPageRoute(builder: (context) => Video(video: this.video)));
+                  },
+                  iconSize: constraint.maxWidth / 6,
+                  icon: Icon(Icons.play_circle_outline),
+                  color: Colors.white,
+                ),
+              )
+            ]
+          ),
         );
       }
     );

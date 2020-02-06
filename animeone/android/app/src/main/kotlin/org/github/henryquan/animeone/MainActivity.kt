8b9b20b75657a058aea164d44a650824dbadaad8
Yiheng (NUC) <development.henryquan@gmail.com>
2020-02-06 21:39:38 +1100

    try to get cookie from device

--- animeone/android/app/src/main/kotlin/org/github/henryquan/animeone/MainActivity.kt
+++ animeone/android/app/src/main/kotlin/org/github/henryquan/animeone/MainActivity.kt
@@ -5,17 +5,25 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.webkit.CookieManager
 
 class MainActivity: FlutterActivity() {
     private val animeOneChannel = "org.github.henryquan.animeone"
+    private val homePage = "https://anime1.me"
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
 
         // Add method channel to receive calls from Flutter side
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, animeOneChannel).setMethodCallHandler { call, result ->
             // Note: this method is invoked on the main thread
-            
+            if (call.method == "bypassBrowserCheck") {
+                // Grab the cookie for anime1.me
+                val cookies: String = CookieManager.getInstance().getCookie(homePage)
+                result.success(cookies)
+            } else {
+                result.notImplemented()
+            }
         }
     }
 }

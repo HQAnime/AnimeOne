af0a8ea81ffc9786d3e31b6a88b2b46678e40849
Yiheng (NUC) <development.henryquan@gmail.com>
2020-02-06 21:33:43 +1100

    added MethodChannel

--- animeone/android/app/src/main/kotlin/org/github/henryquan/animeone/MainActivity.kt
+++ animeone/android/app/src/main/kotlin/org/github/henryquan/animeone/MainActivity.kt
@@ -1,12 +1,21 @@
 package org.github.henryquan.animeone
 
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    private val animeOneChannel = "org.github.henryquan.animeone"
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        // Add method channel to receive calls from Flutter side
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, animeOneChannel).setMethodCallHandler { call, result ->
+            // Note: this method is invoked on the main thread
+            
+        }
     }
 }

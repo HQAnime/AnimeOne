44b2d78e6b6c54e302ef8a63642ce017bbe9b48a
Yiheng <development.henryquan@gmail.com>
2020-02-09 00:02:43 +1100

    load but no images because you need to enter the password

--- animeone/lib/core/anime/AnimeEntry.dart
+++ animeone/lib/core/anime/AnimeEntry.dart
@@ -16,27 +16,30 @@ class AnimeEntry extends AnimeBasic {
   AnimeEntry(Element e) : super.fromJson(null) {
     try {
       // There are rare occasions where you need to enter password
-      List<Element> entryTitle = e.getElementsByClassName('entry-title');
-      if (entryTitle.length > 0) {
-        Node title = entryTitle[0].nodes[0];
-        this.name = title.text;
-        this.link = title.attributes ['href'];
+      Node title = e.getElementsByClassName('entry-title')[0].nodes[0];
+      this.name = title.text;
+      this.link = title.attributes ['href'];
 
-        Node post = e.getElementsByClassName('entry-date')[0];
-        this.postDate = post.text;
+      Node post = e.getElementsByClassName('entry-date')[0];
+      this.postDate = post.text;
 
-        // Get iframe instead
-        var iframe = e.getElementsByTagName('iframe');
-        if (iframe.length > 0) {
-          // There are exceptions where iframe is not used
-          Element video = iframe[0];
-          this.videoLink = new AnimeVideo(video.attributes['src']);
+      // Get iframe instead
+      var iframe = e.getElementsByTagName('iframe');
+      if (iframe.length > 0) {
+        // There are exceptions where iframe is not used
+        Element video = iframe[0];
+        this.videoLink = new AnimeVideo(video.attributes['src']);
+      } else {
+        // Get loadView button
+        var loadBtn = e.getElementsByClassName('loadvideo');
+        if (loadBtn.length > 0) {
+          Element btn = loadBtn[0];
+          this.videoLink = new AnimeVideo(btn.attributes['data-src']);
         } else {
-          // Get loadView button
-          var loadBtn = e.getElementsByClassName('loadvideo');
-          if (loadBtn.length > 0) {
-            Element btn = loadBtn[0];
-            this.videoLink = new AnimeVideo(btn.attributes['data-src']);
+          // See if it needs passwords
+          var password = e.getElementsByClassName('acpwd-container');
+          if (password.length > 0) {
+            // Use need to enter some password
           } else {
             // It is a YouTube preview
             Element youtube = e.getElementsByClassName('youtubePlayer')[0];
@@ -44,20 +47,18 @@ class AnimeEntry extends AnimeBasic {
             this.videoLink = new AnimeVideo(link);
           }
         }
-
-        // Episode links
-        e.getElementsByTagName('a').forEach((n) {
-          if (n.text.contains('全集')) {
-            // Get all episode link
-            this.allEpisodes = GlobalData.domain + n.attributes['href'];
-          } else if (n.text.contains('下一集')) {
-            // Get next episode link
-            this.nextEpisode = GlobalData.domain + n.attributes['href'];
-          }
-        });
-      } else {
-        // You will need to enter the password but I cannot actually encounter this even with vpn
       }
+
+      // Episode links
+      e.getElementsByTagName('a').forEach((n) {
+        if (n.text.contains('全集')) {
+          // Get all episode link
+          this.allEpisodes = GlobalData.domain + n.attributes['href'];
+        } else if (n.text.contains('下一集')) {
+          // Get next episode link
+          this.nextEpisode = GlobalData.domain + n.attributes['href'];
+        }
+      });
     } catch (e) {
       throw new Exception('AnimeEntry - Format changed\n${e.toString()}');
     }

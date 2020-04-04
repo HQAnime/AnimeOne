9e819f18e26dfc71805788e19f90ec678d37c3f7
Yiheng (NUC) <development.henryquan@gmail.com>
2020-04-04 17:35:50 +1100

    update packages?

--- animeone/lib/generated_plugin_registrant.dart
+++ animeone/lib/generated_plugin_registrant.dart
@@ -11,7 +11,6 @@ import 'package:video_player_web/video_player_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));

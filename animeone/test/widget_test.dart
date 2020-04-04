6ed897fe8fcf18e7076f01572fb506c577230d33
Yiheng <development.henryquan@gmail.com>
2020-04-04 17:30:18 +1100

    test doesn't work because the data loading I think

--- animeone/test/widget_test.dart
+++ animeone/test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:animeone/main.dart';
@@ -9,8 +10,22 @@ import 'package:animeone/main.dart';
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 void main() {
-  testWidgets('HomeScreen renders', (WidgetTester tester) async {
+  testWidgets('Basic checks', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
+    await tester.pump(Duration(seconds: 10));
+
+    // Bottom three taps
+    expect(find.byIcon(Icons.list), findsOneWidget);
+    var list = find.byIcon(Icons.new_releases);
+    expect(list, findsOneWidget);
+    expect(find.byIcon(Icons.calendar_today), findsOneWidget);
+    // Refresh
+    expect(find.byIcon(Icons.refresh), findsOneWidget);
+    // Tap new release
+    await tester.tap(list);
+    await tester.pump();
+    // Search button should be visible
+    expect(find.byIcon(Icons.search), findsOneWidget);
   });
 }

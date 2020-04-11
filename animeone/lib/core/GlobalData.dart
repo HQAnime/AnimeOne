823c8fc42b0ebc2ba0349c2b4cf854311f9fecd5
Yiheng <development.henryquan@gmail.com>
2020-04-11 16:12:50 +1000

    1.0.9

--- animeone/lib/core/GlobalData.dart
+++ animeone/lib/core/GlobalData.dart
@@ -19,7 +19,7 @@ import 'anime/AnimeInfo.dart';
 class GlobalData {
 
   static final domain = 'https://anime1.me/';
-  static final version = '1.0.8';
+  static final version = '1.0.9';
 
   static final githubRelease = 'https://raw.githubusercontent.com/HenryQuan/AnimeOne/api/app.json';
   static final latestRelease = 'https://github.com/HenryQuan/AnimeOne/releases/latest';

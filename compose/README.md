b37e8ec0768ba4c8613131ccd6c2bf6f54932048
Yiheng <development.henryquan@gmail.com>
2022-03-12 16:00:30 +1100

    added all material icons

--- compose/README.md
+++ compose/README.md
@@ -1,8 +1,13 @@
 # Compose
 This is the experimental Jetpack Compose version.
 
+# Material
+Material Design 2 is currently used, and the project should move to Material You since APIs are stable.
+
+## Icons
+- https://fonts.google.com/icons?selected=Material+Icons
+
 ## Navigation Rail
 - https://devblogs.microsoft.com/surface-duo/jetpack-compose-navigation-rail/
 - https://material.io/components/navigation-rail
 - [Navigation Rail on Android Developer](https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary#NavigationRail(androidx.compose.ui.Modifier,androidx.compose.ui.graphics.Color,androidx.compose.ui.graphics.Color,androidx.compose.ui.unit.Dp,kotlin.Function1,kotlin.Function1))
-
--- compose/app/src/main/java/org/github/henryquan/animeone/ui/screen/HomeScreen.kt
+++ compose/app/src/main/java/org/github/henryquan/animeone/ui/screen/HomeScreen.kt
@@ -1,19 +1,30 @@
 package org.github.henryquan.animeone.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.CalendarToday
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.NewReleases
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import org.github.henryquan.animeone.ui.theme.AnimeOneTheme
 
 @Composable
 fun HomeScreen(
 ) {
+    val navController = rememberNavController()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -23,32 +34,37 @@ fun HomeScreen(
     ) {
         Row {
             NavigationRail(
-                header = {
-                    // TODO: add the logo here or in content??
-                    Text("LOGO")
-                },
                 content = {
+                    // TODO: update the logo here
+                    Text("LOGO")
+                    // TODO: ideally, there should be a line extending the icon
                     // This moves all items down
-                    Spacer(modifier = Modifier.weight(1f))
+                    Spacer(
+                        modifier = Modifier
+                            .weight(1f)
+                            .width(4.dp)
+                            .clip(CircleShape)
+                            .background(Color.Black)
+                    )
                     NavigationRailItem(
                         selected = false,
                         onClick = {},
                         label = { Text("時間表") }, icon = {
-                            Icon(Icons.Default.Search, "時間表")
+                            Icon(Icons.Default.CalendarToday, "時間表")
                         }
                     )
                     NavigationRailItem(
                         selected = false,
                         onClick = {},
                         label = { Text("動畫列表") }, icon = {
-                            Icon(Icons.Default.Search, "動畫列表")
+                            Icon(Icons.Default.List, "動畫列表")
                         }
                     )
                     NavigationRailItem(
                         selected = false,
                         onClick = {},
                         label = { Text("最新") }, icon = {
-                            Icon(Icons.Default.Search, "最新")
+                            Icon(Icons.Default.NewReleases, "最新")
                         }
                     )
                     // Uncomment this to make it centered

0fd296c70e19cb5caf30794bf5b8bfcea8e4fc59
Yiheng <development.henryquan@gmail.com>
2022-03-13 00:31:01 +1100

    try using box for navigation

--- compose/app/src/main/java/org/github/henryquan/animeone/ui/screen/home/HomeScreen.kt
+++ compose/app/src/main/java/org/github/henryquan/animeone/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package org.github.henryquan.animeone.ui.screen.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
@@ -9,8 +10,7 @@ import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.NewReleases
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,10 +31,7 @@ sealed class HomeTabs(val route: String, val title: String, val icon: ImageVecto
 @Composable
 fun HomeScreen(
 ) {
-    val navController = rememberNavController()
-    val state by navController.currentBackStackEntryAsState()
-    val currentRoute = state?.destination?.route
-
+    var currentScreen by remember { mutableStateOf(HomeTabs.About.route) }
     val tabs = listOf(HomeTabs.About, HomeTabs.Schedule, HomeTabs.AnimeList, HomeTabs.Latest)
 
     Scaffold() {
@@ -45,10 +42,10 @@ fun HomeScreen(
                     // Push all tabs down
                     Spacer(modifier = Modifier.weight(1f))
                     tabs.forEach { tab ->
-                        val tabRoute = tab.route
+                        val tabName = tab.route
                         NavigationRailItem(
-                            selected = currentRoute == tabRoute,
-                            onClick = { navController.navigate(tabRoute) },
+                            selected = currentScreen == tabName,
+                            onClick = { currentScreen = tabName },
                             label = { Text(tab.title) }, icon = {
                                 Icon(tab.icon, tab.title)
                             }
@@ -59,11 +56,11 @@ fun HomeScreen(
                 }
             )
 
-            NavHost(navController = navController, startDestination = HomeTabs.About.route) {
-                composable(HomeTabs.Latest.route) { LatestScreen() }
-                composable(HomeTabs.AnimeList.route) { AnimeListScreen() }
-                composable(HomeTabs.Schedule.route) { ScheduleScreen() }
-                composable(HomeTabs.About.route) { AboutScreen() }
+            Box {
+                LatestScreen()
+                AnimeListScreen()
+                ScheduleScreen()
+                AboutScreen()
             }
         }
     }

cae0ae61cb5323367c2c8b842af9c63aa11807c8
Yiheng <development.henryquan@gmail.com>
2022-03-13 18:03:24 +1100

    added more libraries for http requests

--- compose/app/src/main/java/org/github/henryquan/animeone/service/AnimeOneService.kt
+++ compose/app/src/main/java/org/github/henryquan/animeone/service/AnimeOneService.kt
@@ -1,6 +1,25 @@
 package org.github.henryquan.animeone.service
 
+import com.github.kittinunf.fuel.Fuel
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class AnimeOneService : BaseService() {
 
+
+    suspend fun getLatestAnimeList(): String? {
+        return suspendCoroutine { completion ->
+            Fuel.get("https://anime1.me/%e7%95%99%e8%a8%80%e6%9d%bf")
+                .timeout(10000)
+                .responseString { _, _, result ->
+                    result.fold(
+                        success = {
+                            completion.resume(it)
+                        }, failure = {
+                            completion.resume(null)
+                        }
+                    )
+                }
+        }
+    }
 }
--- compose/app/src/main/java/org/github/henryquan/animeone/ui/screen/home/LatestScreen.kt
+++ compose/app/src/main/java/org/github/henryquan/animeone/ui/screen/home/LatestScreen.kt
@@ -3,13 +3,20 @@ package org.github.henryquan.animeone.ui.screen.home
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.lifecycle.viewmodel.compose.viewModel
+import org.github.henryquan.animeone.service.AnimeOneService
 import org.github.henryquan.animeone.viewmodel.home.LatestViewModel
 
 @Composable
 fun LatestScreen(
     viewModel: LatestViewModel = viewModel()
 ) {
+    LaunchedEffect("get latest anime") {
+        val service = AnimeOneService()
+        println(service.getLatestAnimeList())
+    }
+
     Surface {
         Text("Latest Anime List")
     }

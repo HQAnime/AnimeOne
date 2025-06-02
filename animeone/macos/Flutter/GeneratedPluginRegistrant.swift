5600f4ce45b4e110ab7b64fdf4204a5dbf496506
Yiheng <development.henryquan@gmail.com>
2025-06-02 20:42:09 +1000

    moved to C drive due to D drive issue
    This is likely due to issue because the cache is in the C drive not D drive where I put my project

--- animeone/macos/Flutter/GeneratedPluginRegistrant.swift
+++ animeone/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import shared_preferences_foundation
+import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
--- animeone/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ animeone/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/henryquan/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/henryquan/Documents/GitHub/AnimeOne/animeone"
+export "FLUTTER_ROOT=C:\libs\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\GitHub\AnimeOne\animeone"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.1.8"
